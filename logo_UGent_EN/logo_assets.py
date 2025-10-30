bac7b1755fc1e38cb2289627563249a797128746
Moamen Mohamed Gamal Moustafa <momenmohamed82@gmail.com>
2025-10-30 12:19:43 +0100

    update

---
+++ logo_UGent_EN/logo_assets.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_LOGO_DIR = os.path.dirname(os.path.abspath(__file__))
+
+UGENT_LOGO_MAIN_EN = os.path.join(BASE_LOGO_DIR, "logo_UGent_EN", "logo_UGent_EN_RGB_2400_color-on-white.png")
+
+FACULTY_LOGOS = {
+    "FW": os.path.join(BASE_LOGO_DIR, "Faculteit Farmaceutische Wetenschappen (FW)", "icon_UGent_FW_EN_RGB_2400_color-on-white.png"),
+    # Add other faculties in the future as needed
+}
